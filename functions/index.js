dff09cdca2381c725e14819824c1636a61cb128d
Todd Tyree <tatyree@gmail.com>
2019-06-03 13:14:15 +0100

    Create startScenario and finishScenario functions
    These are the baseline writers for the Forms-based QTs.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,46 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const firestore = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const createRecord = async (record, collection) => {
+  console.info({ collection: "received" });
+
+  const titleFormat = /^(.+)\s*:\s*([^:]+)$/;
+  testDetails = record.title.match(titleFormat);
+  if (testDetails === null) {
+    throw new Error("Title is incorrectly formatted " + record.title);
+  } else {
+    record.testName = testDetails[1];
+    record.testPhase = testDetails[2];
+  }
+
+  record.createdAt = new Date();
+  await firestore
+    .collection(collection)
+    .doc()
+    .set(record);
+  return true;
+}
+
+exports.scenarioStart = functions.https.onRequest((request, response) => {
+  return createRecord(request.body, "startScenario")
+    .then(() => {
+      return response.status(200).send({status: 'OK'});
+    })
+    .catch((e) => {
+      console.error(e);
+      return response.status(500).send({status: 'Error'});
+    });
+});
+
+exports.finishScenario = functions.https.onRequest((request, response) => {
+  return createRecord(request.body, "finishScenario")
+    .then(() => {
+      return response.status(200).send({status: 'OK'});
+    })
+    .catch((e) => {
+      console.error(e);
+      return response.status(500).send({status: 'Error'});
+    });
+});
