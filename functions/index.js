7a5b3af6cb81755ab6ece7f4499d63d49f492e3a
Todd Tyree <tatyree@gmail.com>
2019-09-13 17:24:37 +0100

    WIP: building timing records

--- functions/index.js
+++ functions/index.js
@@ -2,12 +2,13 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 const firestore = admin.firestore();
+const increment = admin.firestore.FieldValue.increment(1);
 const NotifyClient = require('notifications-node-client').NotifyClient;
 
 const sendEmail = async (record) => {
+  if (record.template === undefined) return;
   if (record.simpleKey !== functions.config().simple.key) {
-    console.warn('Attempted unauthorised use');
-    return false;
+    console.warn(`Attempted unauthorised use ${record.simpleKey}`);
   } else {
     console.info({ sendingEmail: record.email });
 
@@ -15,18 +16,57 @@ const sendEmail = async (record) => {
     await client.sendEmail(record.template, record.email, {});
 
     console.info({ sentEmail: record.email });
-    return true;
   }
 }
 
-exports.sendEmail = functions.https.onRequest((request, response) => {
-  return sendEmail(request.body, "startScenario")
-    .then(() => {
-      return response.status(200).send({status: 'OK'});
-    })
-    .catch((e) => {
-      console.error(e);
-      return response.status(500).send({status: 'Error'});
-    });
+const writeRecord = async (record) => {
+  if (record.simpleKey !== functions.config().simple.key) {
+    console.warn(`Attempted unauthorised use ${record.simpleKey}`);
+    record.badSimpleKey = record.simpleKey;
+  }
+  if (typeof record.submittedAt === 'string') record.submittedAt = new Date(record.submittedAt);
+  delete record.simpleKey;
+  record.createdAt = new Date();
+  await firestore.collection('submissions').add(record);
+  return true;
+};
+
+exports.sendEmail = functions.https.onRequest(async (request, response) => {
+  await sendEmail(request.body);
+  await writeRecord(request.body);
+  return response.status(200).send({status: 'OK'})
 });
 
+const writeTiming = async (data) => {
+	console.log({data});
+  // await firestore.collection('submissions').doc(data.email).set(data, { merge: true });
+  return true;
+};
+
+exports.updateTimings = functions.firestore.document('submissions/{submissionId}').onCreate(async (snap, context) => {
+	const submission = await snap.data();
+	const email = submission.email;
+	const exercise = submission.exercise;
+  const phase = submission.phase;
+  const phaseCountKey = `${phase}Count`;
+	const timing = {
+		email,
+		exercise,
+	};
+	timing[phase] =  submission.submittedAt;
+  timing[phaseCountKey] = increment;
+
+  const timingDoc = await firestore.collection('timings').doc(email).get();
+
+  if (timingDoc.exists) {
+    const existingData = timingDoc.data();
+    // They've already submitted this once.
+    if (existingData[phaseCountKey] !== undefined) {
+      await firestore.collection('timings').doc(email).set({ [phaseCountKey]: increment }, { merge: true });
+      return
+    }
+  } else {
+    await firestore.collection('timings').doc(email).set(timing, { merge: true });
+  }
+	return true
+});
